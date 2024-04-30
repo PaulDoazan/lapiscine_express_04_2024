1907747c77243bf90793594156b907327d41909d
pdoazan <paul.doazan@gmail.com>
2024-04-30 17:12:48 +0200

    Remove require morgan

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const morgan = require('morgan')
+// const morgan = require('morgan')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
 
