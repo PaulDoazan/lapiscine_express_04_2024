ab1e715ccdd0e467970b83ef3f8e88bbce75feb8
pdoazan <paul.doazan@gmail.com>
2024-04-30 17:10:05 +0200

    Really ready for production

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express()
 
 const port = process.env.PORT || 5000
 
-require("./db/sequelizeSetup")
+// require("./db/sequelizeSetup")
 
 console.log(process.env.NODE_ENV)
 
@@ -24,17 +24,17 @@ if (process.env.NODE_ENV === "development") {
     app.use(morgan('dev'))
 }
 
-const coworkingRouter = require('./routes/coworkingRoutes')
-const userRouter = require('./routes/userRoutes')
-const reviewRouter = require('./routes/reviewRoutes')
+// const coworkingRouter = require('./routes/coworkingRoutes')
+// const userRouter = require('./routes/userRoutes')
+// const reviewRouter = require('./routes/reviewRoutes')
 
 app.get('/', (req, res) => {
     res.json({ message: 'Hello World!' })
 })
 
-app.use('/api/coworkings', coworkingRouter)
-app.use('/api/users', userRouter)
-app.use('/api/reviews', reviewRouter)
+// app.use('/api/coworkings', coworkingRouter)
+// app.use('/api/users', userRouter)
+// app.use('/api/reviews', reviewRouter)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
