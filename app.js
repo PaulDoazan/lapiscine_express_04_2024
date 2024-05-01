b39f28579ce48c50cf991414015e58d40a7d0715
pdoazan <paul.doazan@gmail.com>
2024-05-01 23:34:34 +0200

    Reintroduce routes

--- app.js
+++ app.js
@@ -24,17 +24,17 @@ if (process.env.NODE_ENV === "development") {
     app.use(morgan('dev'))
 }
 
-// const coworkingRouter = require('./routes/coworkingRoutes')
-// const userRouter = require('./routes/userRoutes')
-// const reviewRouter = require('./routes/reviewRoutes')
+const coworkingRouter = require('./routes/coworkingRoutes')
+const userRouter = require('./routes/userRoutes')
+const reviewRouter = require('./routes/reviewRoutes')
 
 app.get('/', (req, res) => {
     res.json({ message: 'Hello World!' })
 })
 
-// app.use('/api/coworkings', coworkingRouter)
-// app.use('/api/users', userRouter)
-// app.use('/api/reviews', reviewRouter)
+app.use('/api/coworkings', coworkingRouter)
+app.use('/api/users', userRouter)
+app.use('/api/reviews', reviewRouter)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
